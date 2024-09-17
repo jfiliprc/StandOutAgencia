f485198a7630d810f7180bc6983866db7aa8f7b7
J. Felipe Rocha <109008096+jfiliprc@users.noreply.github.com>
2024-09-16 22:39:52 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,21 +24,8 @@
             $('.navbar').removeClass('sticky-top shadow-sm');
         }
     });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
 
+    
 
     // Skills
     $('.skill').waypoint(function () {
@@ -83,12 +70,14 @@
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
     });
-    $('#portfolio-flters li').on('click', function () {
+    $('#portfolio-flters li').on('click', function (e) {
+        e.preventDefault();  // Prevents page from jumping to top or reloading
         $("#portfolio-flters li").removeClass('active');
         $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
+    
     
 })(jQuery);
 
